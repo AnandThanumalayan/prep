64fbdf9d721ec895bd0b576e27302fbdff3a4448
Anand Thanumalayan <anandthanumalayan@Anands-MacBook-Pro.local>
2022-02-24 08:55:50 +0530

    solution 1

--- p1-subseq-s1/Program.cs
+++ p1-subseq-s1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class Program {
+	public static bool IsValidSubsequence(List<int> array, List<int> sequence) {
+		// Write your code here.
+		int sequenceLength = sequence.Count;
+		List<int> subsequence = new List<int>(sequenceLength);
+		foreach(int s in sequence){
+            if(array.Contains(s) && !subsequence.Contains(s)){
+                subsequence.Add(s);
+            }
+		}
+  	bool isSubSequence = subsequence.Count == sequence.Count;
+		return isSubSequence;
+	}
+}
