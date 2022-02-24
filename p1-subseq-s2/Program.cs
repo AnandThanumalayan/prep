6e30963a25b8dc6d4297ca7d9c0bc0ccdb268daf
Anand Thanumalayan <anandthanumalayan@Anands-MacBook-Pro.local>
2022-02-24 08:52:47 +0530

    solution 2

---
+++ p1-subseq-s2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program {
+	public static bool IsValidSubsequence(List<int> array, List<int> sequence) {
+		// find the first occurence of sequence[0] in the array
+		var firstOccurenceInArray = array.IndexOf(sequence[0]);
+
+		// if the first item itself is not found, return
+		if(firstOccurenceInArray < 0){
+			return false;
+		}
+
+		// int maxLengthToCheck = firstOccurenceInArray + sequence.Count;
+		// // iterate through the sequence list starting at the first occurence
+		// for(int i=firstOccurenceInArray; i < maxLengthToCheck; i++){
+		// 	if(array[i] == sequence[0]){
+		// 		// increment the starting counter i ??
+		// 	}
+		// 	if(sequence[i] != array[i]){
+		// 		return false;
+		// 	}
+		// }
+
+		int i = 0;
+		i = firstOccurenceInArray;
+		int maxLengthToCheck = i + sequence.Count;
+		while(i < maxLengthToCheck){
+			if(array[i] == sequence[0]){
+				// increment the max length
+				maxLengthToCheck++;
+			}
+			if(sequence[i] != array[i]){
+				return false;
+			}
+			i++;
+		}
+		return false;
+	}
+}
