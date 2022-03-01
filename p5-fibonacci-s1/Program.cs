63bb451f21d2b7e339dd0b861321e1a51196ed47
Anand Thanumalayan <anandthanumalayan@Anands-MacBook-Pro.local>
2022-03-01 11:33:30 +0530

    p5 fibonacci solution

--- p5-fibonacci-s1/Program.cs
+++ p5-fibonacci-s1/Program.cs
@@ -5,10 +5,12 @@ using System.IO;
 class Solution {
     
     public static int Fibonacci(int n) {
-
-
         // Write your code here.
-        
+        if(n == 0 || n == 1){
+            return n;
+        }
+        int fibonacci = Fibonacci(n-1) + Fibonacci(n-2);
+        return fibonacci;
     }
 
     static void Main(String[] args) {
