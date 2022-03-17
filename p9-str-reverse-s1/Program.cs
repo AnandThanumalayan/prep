4c37387fcb1193ecc83b76e2e9e5ebfa671e8e60
Anand Thanumalayan <anand.thanumalayan@Aspiresys.com>
2022-03-17 19:38:30 +0530

    p9 solution commit

---
+++ p9-str-reverse-s1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var str = "hello world";
+var charArray = str.ToCharArray();
+
+// recursion bootstrapping
+printReverse(charArray, charArray.Length - 1);
+
+static void printReverse(char[] charArray, int remainingLength){
+// recursion end condition
+if(remainingLength < 0){
+    return;
+}
+Console.Write(charArray[remainingLength]);
+remainingLength = remainingLength - 1;
+
+// recursion initiation
+printReverse(charArray, remainingLength);
+}
