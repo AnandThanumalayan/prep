c4a285bc9862408ac01bece18fa91c5a9f5161d3
Anand Thanumalayan <anand.thanumalayan@Aspiresys.com>
2022-03-18 19:54:47 +0530

    p10 inplace string reverse solution

--- p10-inplace-str-reverse-s1/Program.cs
+++ p10-inplace-str-reverse-s1/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var s = new string[] {"A"," ","m","a","n",","," ","a"," ","p","l","a","n",","," ","a"," ","c","a","n","a","l",":"," ","P","a","n","a","m","a"};
+var charArray = s;
+int counter = 0;
+
+// recursion bootstrapping
+reverseString(charArray, counter);
+
+for(int i = 0; i < s.Length; i++){
+    Console.WriteLine($"{i} : {s[i]} : {s.Length -1 - i}");
+}
+
+static void reverseString(string[] charArray, int currentCtr){
+    int totalLength = charArray.Length - 1;
+    int frontCtr = currentCtr;
+    int backCtr = totalLength - currentCtr;
+
+    // recursion end condition - stop when the counters reach the middle
+    // of a even number sized or odd number sized array
+    if(backCtr - frontCtr <= 0){
+        return;
+    }
+    string swapChar;
+    
+    // swapping the characters
+    swapChar = charArray[frontCtr];
+    charArray[frontCtr] = charArray[backCtr];
+    charArray[backCtr] = swapChar;
+
+    currentCtr = currentCtr + 1;
+
+    reverseString(charArray, currentCtr);
+}
+
+// var charArray = s.ToCharArray();
+// int counter = 0;
+
+// // recursion bootstrapping
+// reverseString(charArray, counter);
+
+// static void reverseString(char[] charArray, int currentCtr){
+//     int totalLength = charArray.Length - 1;
+//     int frontCtr = currentCtr;
+//     int backCtr = totalLength - currentCtr;
+
+//     // recursion end condition - stop when the counters reach the middle
+//     // of a even number sized or odd number sized array
+//     if(backCtr - frontCtr <= 1){
+//         return;
+//     }
+//     char swapChar;
+    
+//     // swapping the characters
+//     swapChar = charArray[frontCtr];
+//     charArray[frontCtr] = charArray[backCtr];
+//     charArray[backCtr] = swapChar;
+
+//     currentCtr = currentCtr + 1;
+
+//     reverseString(charArray, currentCtr);
+// }
